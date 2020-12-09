6d47c2908e533e4a6759562ec928016ea1dc5526
Yauhen Davidovich <davidovichyauhen@gmail.com>
2020-12-09 21:30:57 +0300

    feat: finish HW1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import styles from './Message.module.css'
 
-function Message() {
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
-
+        <div className={styles.wrapper}>
+            <img className={styles.avatar} src={props.avatar} alt="avatar"/>
+            <div className={styles.message__wrapper}>
+                <div>
+                    <p className={styles.name}>{props.name}</p>
+                    <p className={styles.message__text}>{props.message}</p>
+                </div>
+                <time className={styles.time} dateTime={"props.time"}>{props.time}</time>
+            </div>
         </div>
     );
 }
